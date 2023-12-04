df29e72ca15776dda99636c60f1494609ff6afdd
norbusonam <nsonam1822@gmail.com>
2023-12-04 17:08:33 -0500

    first guess

--- 03_guessing_game/src/main.rs
+++ 03_guessing_game/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // introduce game
+    println!("Number Guessing Game");
+
+    // prompt user for guess
+    let mut guess = String::new();
+    println!("Guess a number:");
+
+    // read user input
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // clean up user input
+    guess = guess.trim().to_string();
+
+    // tell user what they guessed
+    println!("You guessed {guess}");
 }
