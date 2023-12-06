0108953a5a772d0bb480fb95f285f77dfded782f
norbusonam <nsonam1822@gmail.com>
2023-12-06 18:57:47 -0500

    game loop

--- 03_guessing_game/src/main.rs
+++ 03_guessing_game/src/main.rs
@@ -6,24 +6,28 @@ fn main() {
     // introduce game
     println!("Number Guessing Game");
 
+    // generate random number
     let random_number = rand::thread_rng().gen_range(1..101);
 
-    // prompt user for guess
-    let mut guess = String::new();
-    println!("Guess a number:");
+    loop {
+        // ask user for input
+        println!("Guess a number:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // read user input
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // convert user input to number
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    // convert user input to number
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    // tell the user if the guess was right
-    match guess.cmp(&random_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // tell the user if the guess was right
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
